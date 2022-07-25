a608e3069d760125bad06b58bdc93d4bccf5d0e6
YR-Johnson <eljonybarra@gmail.com>
2022-07-25 06:16:35 -0300

    add animaciones

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3030;
 
-app.use(express.static('public'));
+//routes
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")));
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, "views", "register.html")));
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")));
 
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')));
-app.listen(port,()=>console.log('Server running in http://localhost:'+port));
+//add static
+app.use(express.static("public"));
+
+//init server
+app.listen(port, () => console.log("Server running in http://localhost:" + port));
